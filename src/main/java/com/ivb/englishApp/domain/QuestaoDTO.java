bae319217edee6b68f1302e988dec78aaf3c52a3
wbaronic <wbaronic@gmail.com>
2025-07-02 09:14:42 -0300

    class para record

--- src/main/java/com/ivb/englishApp/domain/QuestaoDTO.java
+++ src/main/java/com/ivb/englishApp/domain/QuestaoDTO.java
@@ -3,66 +3,28 @@ package com.ivb.englishApp.domain;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class QuestaoDTO {
+public record QuestaoDTO(Long id,String describe,String optionA,String optionB,String correctOption) {
 
-    private Long id;
-    private String describe;
-    private String optionA;
-    private String optionB;
-    private String correctOption;
    // private List<QuestaoExplanationDTO> explanations;
 
-    public QuestaoDTO(Questao questao) {
-        this.id = questao.getId();
-        this.describe = questao.getDescribe();
-        this.optionA = questao.getOptionA();
-        this.optionB = questao.getOptionB();
-        this.correctOption = questao.getCorrectOption();
-       // this.explanations = questao.getExplanations()
-//                                    .stream()
-//                                    .map(QuestaoExplanationDTO::new)
-//                                    .collect(Collectors.toList());
+
+    public QuestaoDTO {
     }
 
-	public Long getId() {
-		return id;
-	}
+    public QuestaoDTO(Questao questao) {
+        this(
+                questao.getId(),
+                questao.getDescribe(),
+                questao.getOptionA(),
+                questao.getOptionB(),
+                questao.getCorrectOption()
+                // questao.getExplanations()
+                //       .stream()
+                //       .map(QuestaoExplanationDTO::new)
+                //       .collect(Collectors.toList())
+        );
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDescribe() {
-		return describe;
-	}
-
-	public void setDescribe(String describe) {
-		this.describe = describe;
-	}
-
-	public String getOptionA() {
-		return optionA;
-	}
-
-	public void setOptionA(String optionA) {
-		this.optionA = optionA;
-	}
-
-	public String getOptionB() {
-		return optionB;
-	}
-
-	public void setOptionB(String optionB) {
-		this.optionB = optionB;
-	}
-
-	public String getCorrectOption() {
-		return correctOption;
-	}
-
-	public void setCorrectOption(String correctOption) {
-		this.correctOption = correctOption;
-	}
 
   
     
